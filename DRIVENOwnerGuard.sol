330ffd90a65741c6591c53f04969ba92c571f498
DRIVENpol <84129217+polthemarketer@users.noreply.github.com>
2021-12-21 04:06:12 +0200

    Add examples

--- DRIVENOwnerGuard.sol
+++ DRIVENOwnerGuard.sol
@@ -86,6 +86,33 @@ contract DRIVENOwnerGuard {
 
 
 // START YOUR SMART CONTRACT HERE
-// ...
+
+contract myProject is DRIVENOwnerGuard {
+
+// WRITE YOUR SMART CONTRACT
 
 }
+
+}
+
+
+
+
+/* ==== EXAMPLES
+
+1) require(approve == true) AND require(isBlocked[msg.sender] == false)
+Use this sintax on very-important functions of your own smart contract + onlyOwner modifier
+I.E.: On functions that will change the tokenomics, on functions that will allow or not swapping
+
+DON'T USE IT ON AUTOMATIC FUNCTIONS (DIVIDENDS REDISTRIBUTION)
+
+
+2) require(isBlocked[msg.sender] == false)
+Use this sintax on functions like "transferFrom" or "claimDividends" so when one of your holders is hacked
+you can simply mark the hacker's address as blocked and they will not be able to use
+their tokens.
+
+3) isBlocked[msg.sender] or setStatusForAddress function
+Use this sintax / function when you want to blacklist an addres (as we did on the allowChanges function)
+
+*/
